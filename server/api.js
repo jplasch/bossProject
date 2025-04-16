878c146a6d3b5aea73d76931a017ff13e2007385
jplasch <64796650+jplasch@users.noreply.github.com>
2025-04-16 14:18:03 -0700

    Add comment for clarity

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// Mount minionsRouter to apiRouter
 const minionsRouter = require('./minionsRouter');
 apiRouter.use('/minions', minionsRouter);
 
