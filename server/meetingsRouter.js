25b402cac9adf05b56ba29d4d0fc70f1da6ccb1f
jplasch <64796650+jplasch@users.noreply.github.com>
2025-04-16 16:39:43 -0700

    Add entire GET/POST/DELETE functionality to meetingsRouter

--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -4,6 +4,25 @@ const db = require('./db');
 
 // meetings router
 const router = express.Router();
+const meetings = 'meetings';
 
+// GET request
+router.get('/', (req, res, next) => {
+    const allMeetings = db.getAllFromDatabase(meetings);
+    res.send(allMeetings);
+});
+
+// POST request
+router.post('/', (req, res, next) => {
+    const meeting = db.createMeeting();
+    const createdMeeting = db.addToDatabase(meetings, meeting);
+    res.status(201).send(createdMeeting);
+});
+
+// DELETE request
+router.delete('/', (req, res, next) => {
+    db.deleteAllFromDatabase(meetings);
+    res.status(204).send();
+});
 
 module.exports = router;
