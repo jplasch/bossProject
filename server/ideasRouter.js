a75e4f5bf7bc0922aaa3560ab6d9de284532bfb0
jplasch <64796650+jplasch@users.noreply.github.com>
2025-04-16 16:19:05 -0700

    Add POST and DELETE functionality to ideasRouter

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -27,4 +27,48 @@ router.get('/:ideaId', (req, res, next) => {
     res.send(idea);
 });
 
+// PUT requests
+router.put('/:ideaId', (req, res, next) => {
+    const idea = req.body;
+    const id = req.params.ideaId;
+
+    if (isNaN(id) === true) {
+        return res.status(404).send('Not a number');
+    }
+
+    const updatedIdea = db.updateInstanceInDatabase(ideas, idea);
+    if (updatedIdea === null) {
+        return res.status(404).send('Invalid ID');
+    }
+
+    res.send(updatedIdea);
+});
+
+// POST request
+router.post('/', (req, res, next) => {
+    const idea = req.body;
+    const createdIdea = db.addToDatabase(ideas, idea);
+
+    if (createdIdea !== undefined) {
+        return res.status(201).send(createdIdea);
+    }
+});
+
+// DELETE request
+router.delete('/:ideaId', (req, res, next) => {
+    const id = req.params.ideaId;
+
+    if (isNaN(id) === true) {
+        return res.status(404).send('Not a number');
+    }
+
+    const deletedIdea = db.deleteFromDatabasebyId(ideas, id);
+
+    if (deletedIdea === false) {
+        return res.status(404).send('Invalid ID');
+    }
+
+    res.status(204).send(deletedIdea);
+});
+
 module.exports = router;
