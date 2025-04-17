5cc941d99508bdbafb3313f50d868492a9c222a6
jplasch <64796650+jplasch@users.noreply.github.com>
2025-04-16 20:06:28 -0700

    Finish GET functionality for workRouter; 100% complete

--- server/workRouter.js
+++ server/workRouter.js
@@ -9,7 +9,17 @@ const work = 'work';
 router.get('/', (req, res, next) => {
     const minionId = req.params.minionId;
     const allWork = db.getAllFromDatabase(work);
+
+    if (isNaN(minionId) === true) {
+        return res.status(404).send('Not a number');
+    }
+
     const minionWork = allWork.filter(work => work.minionId === minionId);
+
+    if (minionWork.length === 0) {
+        return res.status(404).send('Invalid ID');
+    }
+
     res.send(minionWork);
 });
 
